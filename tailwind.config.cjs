9e56fe0c4134c022fae95a30c3f9e481007c64ab
Felipe Marin <lfelipe.marin@gmail.com>
2023-05-19 13:53:20 -0500

    chore: extended tw classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        128: '32rem',
+        256: 'calc(100vh - 100px)'
+      },
+      flex: {
+        50: '1 1 50%'
+      }
+    },
     screens: {
       xs: '475px',
       ...defaultTheme.screens
