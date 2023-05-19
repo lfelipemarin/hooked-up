928c5ece42cd9c705c1f4d8135b8dc5f826cd7af
Felipe Marin <lfelipe.marin@gmail.com>
2023-05-19 17:37:02 -0500

    chore: tailwind config to work with ts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
