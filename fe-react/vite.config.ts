b7021f4df649657d8bce7a88c3996fb562507961
Felipe Marin <lfelipe.marin@gmail.com>
2025-01-17 16:27:00 -0500

    Add CI

--- fe-react/vite.config.ts
+++ fe-react/vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/hooked-up/',
   test: {
     globals: true
   }
